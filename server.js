ce24befdcefa45d498ff4fef3ddbe0019cec158d
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-08-21 10:29:05 -0400

    working on server.js

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ var PORT = 3000;
 var app = express();
 
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/27017", {useNewURLParser: true});
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
-app.get("/scrape", function(req, res) {
-    axios.get("http://www.theverge.com/").then(function(response) {
+app.get("/scrape", function (req, res) {
+    axios.get("http://www.theverge.com/").then(function (response) {
         var $ = cheerio.load(response.data);
 
-        $("").each(function(i, element) {
+        $("page h2").each(function (i, element) {
             var result = {};
 
             result.healine = $(this).children("a").text();
@@ -27,13 +27,65 @@ app.get("/scrape", function(req, res) {
             result.url = $(this).children("a").attr("href");
 
             db.Page.create(result)
-            .then(function(dbPage) {
-                console.log(dbPage);
-            })
-            .catch(function(err) {
-                console.log(err);
-            });
+                .then(function (dbPage) {
+                    console.log(dbPage);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
         });
         res.send("scrape complete");
     });
 });
+
+app.get("/pages", function (req, res) {
+    db.Page.find({})
+        .then(function (dbPage) {
+            res.json(dbPage);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.get("/pages/summary", function (req, res) {
+    db.Page.findOne(req.body)
+        .populate("summary")
+        .then(function (dbPage) {
+            res.json(dbPage);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.get("/pages", function (req, res) {
+    db.Page.find({})
+        .then(function (dbPage) {
+            res.json(dbPage);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.get("/pages/summary", function (req, res) {
+    db.Page.findOne()
+        .populate("summary")
+        .then(function (dbPage) {
+            res.json(dbPage);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.post("/pages/:id", function(req, res) {
+    
+})
+
+
+app.listen(PORT, function () {
+    console.log("App running port " + PORT + "!");
+});
+
